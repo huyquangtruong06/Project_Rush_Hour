054e31f5d7aa7de2982fdfce846034b63b5bef25
HUY QUANG TRUONG <qnnghi120305@gmail.com>
2025-06-26 17:30:46 +0700

    fifth commit

---
+++ define.py
@@ -0,0 +1,21 @@
+# Setting parameters
+GRID_SIZE = 6
+BASE_CELL_SIZE = 80 # Base cell size
+MIN_CELL_SIZE = 40 # Minimum size when zoomed out
+MAX_CELL_SIZE = 160 # Maximum size when zoomed in
+MARGIN = 50
+
+# Calculate initial window size
+initial_cell_size = BASE_CELL_SIZE
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
+# Color
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (200, 200, 200)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
+COLORS = [RED, BLUE, GREEN, YELLOW, (255, 0, 255), (0, 255, 255), (128, 0, 0), (0, 128, 0)]
