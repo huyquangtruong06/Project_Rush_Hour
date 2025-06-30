c94f2af4cadab46f671b40e608db632492ed15af
khoa26 <khoanb2605@gmail.com>
2025-07-01 00:38:57 +0700

    95 %

--- Button.py
+++ Button.py
@@ -1,17 +1,34 @@
 import pygame
 
 class Button:
-    def __init__(self, screen, x, y, image, scale):
-        width = image.get_width()
-        height = image.get_height()
-        self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
-        self.rect = self.image.get_rect(topleft=(x, y))
-        self.rect.topleft = (x, y)
+    def __init__(self, screen, rel_x, rel_y, image, scale):
         self.screen = screen
+        self.image_original = image
+        self.scale = scale
+        self.rel_x = rel_x  # giá trị từ 0 -> 1
+        self.rel_y = rel_y
+
+        self.update_position()
+
+    def update_position(self):
+        screen_width = self.screen.get_width()
+        screen_height = self.screen.get_height()
+
+        width = self.image_original.get_width()
+        height = self.image_original.get_height()
+        self.image = pygame.transform.scale(self.image_original, (int(width * self.scale), int(height * self.scale)))
+
+        button_width = self.image.get_width()
+        button_height = self.image.get_height()
+
+        abs_x = int(self.rel_x * screen_width - button_width // 2)
+        abs_y = int(self.rel_y * screen_height - button_height // 2)
+
+        self.rect = self.image.get_rect(topleft=(abs_x, abs_y))
 
     def draw(self):
+        self.update_position()
         self.screen.blit(self.image, (self.rect.x, self.rect.y))
 
     def is_clicked(self, pos):
         return self.rect.collidepoint(pos)
-
--- Support.py
+++ Support.py
@@ -4,16 +4,16 @@ import pygame
 def select_levels(screen):
 
     buttons = [
-        Button(screen, 175, 50, LEVELS[0].convert_alpha(), 0.5),
-        Button(screen, 425, 50, LEVELS[1].convert_alpha(), 0.5),
-        Button(screen, 175, 125, LEVELS[2].convert_alpha(), 0.5),
-        Button(screen, 425, 125, LEVELS[3].convert_alpha(), 0.5),
-        Button(screen, 175, 200, LEVELS[4].convert_alpha(), 0.5),
-        Button(screen, 425, 200, LEVELS[5].convert_alpha(), 0.5),
-        Button(screen, 175, 275, LEVELS[6].convert_alpha(), 0.5),
-        Button(screen, 425, 275, LEVELS[7].convert_alpha(), 0.5),
-        Button(screen, 175, 350, LEVELS[8].convert_alpha(), 0.5),
-        Button(screen, 425, 350, LEVELS[9].convert_alpha(), 0.5),
+        Button(screen, 0.3, 0.1, LEVELS[0].convert_alpha(), 0.5),
+        Button(screen, 0.7, 0.1, LEVELS[1].convert_alpha(), 0.5),
+        Button(screen, 0.3, 0.3, LEVELS[2].convert_alpha(), 0.5),
+        Button(screen, 0.7, 0.3, LEVELS[3].convert_alpha(), 0.5),
+        Button(screen, 0.3, 0.5, LEVELS[4].convert_alpha(), 0.5),
+        Button(screen, 0.7, 0.5, LEVELS[5].convert_alpha(), 0.5),
+        Button(screen, 0.3, 0.7, LEVELS[6].convert_alpha(), 0.5),
+        Button(screen, 0.7, 0.7, LEVELS[7].convert_alpha(), 0.5),
+        Button(screen, 0.3, 0.9, LEVELS[8].convert_alpha(), 0.5),
+        Button(screen, 0.7, 0.9, LEVELS[9].convert_alpha(), 0.5),
     ]
     selected_level = False
     while not selected_level:
@@ -28,13 +28,13 @@ def select_levels(screen):
             if event.type == pygame.MOUSEBUTTONDOWN:
                 for i in range(len(buttons)):
                     if buttons[i].is_clicked(event.pos):
-                        print(f"Level {i } selected")
+                        print(f"Level {i + 1} selected")
                         return i 
         pygame.display.update()
 
 def start_menu(screen):
-    Start_button = Button(screen, 200, 100, start_button.convert_alpha(), 0.25)
-    Quit_button = Button(screen, 200, 300, quit_button.convert_alpha(), 0.25)
+    Start_button = Button(screen, 0.5, 0.3, start_button.convert_alpha(), 0.25)
+    Quit_button = Button(screen, 0.5, 0.7, quit_button.convert_alpha(), 0.25)
 
     while True:
         screen.fill(WHITE)
